e362d8c6e9e050941229e5dad65dc379e05b8031
rzh4321 <rzh4321@gmail.com>
2024-12-24 20:23:46 -0500

    initial commit

---
+++ src/main/java/com/courtside/api/CourtsideApplication.java
@@ -0,0 +1,13 @@
+package com.courtside.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtsideApplication.class, args);
+	}
+
+}
